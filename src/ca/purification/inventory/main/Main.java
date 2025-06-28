884a068b0034e182120868c6b99fe6abcb694f72
Specyy <121744367+Specyy@users.noreply.github.com>
2025-06-27 23:53:24 -0700

    Update Main.java

--- src/ca/purification/inventory/main/Main.java
+++ src/ca/purification/inventory/main/Main.java
@@ -1,9 +1,5 @@
 package ca.purification.inventory.main;
 
-/*
- * Student name: Alvyn Kang
- */
-
 /**
  * The {@code Main} class is the entry point for the Water Purification Inventory Management application.
  * It initializes the {@link AppContext}, which manages the overall flow and resources of the application,
